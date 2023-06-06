9be9eba106b016b10af949d5f30d1cb80236b43b
main <refabatya@gmail.com>
2023-06-06 10:17:24 +0500

    header

--- header.js
+++ header.js
@@ -4,24 +4,32 @@ function header() {
     let header = document.createElement("header")
     let nav = document.createElement("nav")
     let links = document.createElement("div")
+    let hide = document.createElement("button")
+    let obj = {
+        'Главная': "./client.html",
+        'Мои кошельки': "./wallets.html",
+        'Мои транзакции': "./transactions.html"
+    }
     for (let title of ['Главная', 'Мои кошельки', 'Мои транзакции']) {
         let a = document.createElement("a")
-        a.href = "#"
         a.innerHTML = title
+        a.href = obj[a.innerHTML]
         links.append(a)
     }
     let logOut = document.createElement("button")
     let span = document.createElement("span")
     let img = document.createElement("img")
 
+    hide.classList.add("hide")
     nav.classList.add("container")
     links.classList.add("links")
     logOut.classList.add("log_out")
 
     span.innerHTML = localedData.email
     img.src = "./img/icons/out.png"
+    hide.innerHTML = "X"
 
-    header.append(nav)
+    header.append(nav, hide)
     nav.append(links, logOut)
     logOut.append(span, img)
     body.append(header)
@@ -29,4 +37,14 @@ function header() {
     logOut.onclick = () => {
         location.assign("index.html")
     }
+    hide.onclick = () => {
+        if (hide.innerHTML === "X") {
+            header.style.top = "-11%"
+            hide.innerHTML = "Show links"
+        }else{
+            header.style.top = "0%"
+            hide.innerHTML = "X"
+        }
+    }
+
 }
